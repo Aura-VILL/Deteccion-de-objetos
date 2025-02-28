11461f4c82e2ba9b899edc8690fcb7201760693a
Aura-VILL <nina.nav.anz@gmail.com>
2025-02-27 19:04:24 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var video;
+var detector;
+var label = "Cargando..."
+
+function setup() {
+  createCanvas(400,450);
+  video=createCapture(VIDEO);
+  video.hide();
+  video.
+  createCapture();
+  size(380, 320);
+}
+
+function draw(){
+  ground("#821737");
+  textSize();
+  fill("white");
+  
+}
+
